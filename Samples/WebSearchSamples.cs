498be6179ff243490c5f8433ea5ef32fdb11bce4
Sudhansu Sekhar Panigrahy <sup@microsoft.com>
2017-11-14 12:14:59 -0800

    Update WebSearch Samples

---
+++ Samples/WebSearchSamples.cs
@@ -0,0 +1,235 @@
+﻿namespace bing_search_dotnet.Samples
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Azure.CognitiveServices.Search;
+    using Microsoft.Azure.CognitiveServices.Search.WebSearch;
+    using Microsoft.Azure.CognitiveServices.Search.WebSearch.Models;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    [SampleCollection("WebSearch")]
+    public class WebSearchSamples
+    {
+        [Example("This will look up a single query (Xbox) and print out name and url for first web, image, news and videos results")]
+        public static void WebSearchResultTypesLookup(string subscriptionKey)
+        {
+            var client = new WebSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var webData = client.Web.SearchAsync(query: "Xbox").Result;
+                Console.WriteLine("Searched for Query# \" Xbox \"");
+
+                //WebPages
+                if (webData?.WebPages?.Value?.Count > 0)
+                {
+                    // find the first web page
+                    var firstWebPagesResult = webData.WebPages.Value.FirstOrDefault();
+
+                    if (firstWebPagesResult != null)
+                    {                        
+                        Console.WriteLine("Webpage Results#{0}", webData.WebPages.Value.Count);
+                        Console.WriteLine("First web page name: {0} ", firstWebPagesResult.Name);
+                        Console.WriteLine("First web page URL: {0} ", firstWebPagesResult.Url);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find web results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any Web data..");
+                }
+
+                //Images
+                if (webData?.Images?.Value?.Count > 0)
+                {
+                    // find the first image result
+                    var firstImageResult = webData.Images.Value.FirstOrDefault();
+
+                    if (firstImageResult != null)
+                    {
+                        Console.WriteLine("Image Results#{0}", webData.Images.Value.Count);
+                        Console.WriteLine("First Image result name: {0} ", firstImageResult.Name);
+                        Console.WriteLine("First Image result URL: {0} ", firstImageResult.ContentUrl);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find first image results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any image data..");
+                }
+
+                //News
+                if (webData?.News?.Value?.Count > 0)
+                {
+                    // find the first news result
+                    var firstNewsResult = webData.News.Value.FirstOrDefault();
+
+                    if (firstNewsResult != null)
+                    {
+                        Console.WriteLine("News Results#{0}", webData.News.Value.Count);
+                        Console.WriteLine("First news result name: {0} ", firstNewsResult.Name);
+                        Console.WriteLine("First news result URL: {0} ", firstNewsResult.Url);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find any News results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see first news data..");
+                }
+
+                //Videos
+                if (webData?.Videos?.Value?.Count > 0)
+                {
+                    // find the first video result
+                    var firstVideoResult = webData.Videos.Value.FirstOrDefault();
+
+                    if (firstVideoResult != null)
+                    {
+                        Console.WriteLine("Video Results#{0}", webData.Videos.Value.Count);
+                        Console.WriteLine("First Video result name: {0} ", firstVideoResult.Name);
+                        Console.WriteLine("First Video result URL: {0} ", firstVideoResult.ContentUrl);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find first video results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any video data..");
+                }
+            }
+          
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+            
+        }
+
+        [Example("This will search (Best restaurants in Seattle), verify number of results and print out name and url of first result")]
+        public static void WebResultsWithCountAndOffset(string subscriptionKey)
+        {
+            var client = new WebSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var webData = client.Web.SearchAsync(query: "Best restaurants in Seattle", offset:10, count:20).Result;
+                Console.WriteLine("Searched for Query# \" Best restaurants in Seattle \"");
+
+                if (webData?.WebPages?.Value?.Count > 0)
+                {
+                    // find the first web page
+                    var firstWebPagesResult = webData.WebPages.Value.FirstOrDefault();
+
+                    if (firstWebPagesResult != null)
+                    {
+                        Console.WriteLine("Web Results#{0}", webData.WebPages.Value.Count);
+                        Console.WriteLine("First web page name: {0} ", firstWebPagesResult.Name);
+                        Console.WriteLine("First web page URL: {0} ", firstWebPagesResult.Url);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find first web result!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any Web data..");
+                }             
+            }
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+
+        [Example("This will search (Microsoft) with response filters to news and print details of news")]
+        public static void WebSearchWithResponseFilter(string subscriptionKey)
+        {
+            var client = new WebSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                IList<string> responseFilterstrings = new List<string>() {"news"};
+                var webData = client.Web.SearchAsync(query: "Microsoft", responseFilter: responseFilterstrings).Result;
+                Console.WriteLine("Searched for Query# \" Microsoft \" with response filters \"news\"");
+
+                //News
+                if (webData?.News?.Value?.Count > 0)
+                {
+                    // find the first news result
+                    var firstNewsResult = webData.News.Value.FirstOrDefault();
+
+                    if (firstNewsResult != null)
+                    {
+                        Console.WriteLine("News Results#{0}", webData.News.Value.Count);
+                        Console.WriteLine("First news result name: {0} ", firstNewsResult.Name);
+                        Console.WriteLine("First news result URL: {0} ", firstNewsResult.Url);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find first News results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any News data..");
+                }
+
+            }
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+
+        [Example("This will search (Lady Gaga) with answerCount and promote parameters and print details of answers")]
+        public static void WebSearchWithAnswerCountPromoteAndSafeSearch(string subscriptionKey)
+        {
+            var client = new WebSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                IList<string> promoteAnswertypeStrings = new List<string>() { "videos"};
+                var webData = client.Web.SearchAsync(query: "Lady Gaga", answerCount: 2, promote: promoteAnswertypeStrings, safeSearch: SafeSearch.Strict).Result;
+                Console.WriteLine("Searched for Query# \" Lady Gaga \"");
+
+                if (webData?.Videos?.Value?.Count > 0)
+                {
+                    var firstVideosResult = webData.Videos.Value.FirstOrDefault();
+
+                    if (firstVideosResult != null)
+                    {
+                        Console.WriteLine("Video Results#{0}", webData.Videos.Value.Count);
+                        Console.WriteLine("First Video result name: {0} ", firstVideosResult.Name);
+                        Console.WriteLine("First Video result URL: {0} ", firstVideosResult.ContentUrl);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find videos results!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Didn't see any data..");
+                }
+            }
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+    }
+}
