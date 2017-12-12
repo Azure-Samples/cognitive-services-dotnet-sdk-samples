ae09dff876aa9f327028d588a0d178657e495e08
Sudhansu Sekhar Panigrahy <sup@microsoft.com>
2017-12-11 17:38:46 -0800

    Create Independent Samples for each SDK

---
+++ SearchV7/AllSearch/Samples/ImageSearchSamples.cs
@@ -0,0 +1,299 @@
+﻿namespace bing_search_dotnet.Samples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.Azure.CognitiveServices.Search.ImageSearch;
+    using Microsoft.Azure.CognitiveServices.Search.ImageSearch.Models;
+
+    [SampleCollection("ImageSearch")]
+    public class ImageSearchSamples
+    {
+        [Example("This will search images for (canadian rockies) then verify number of results and print out first image result, pivot suggestion, and query expansion")]
+        public static void ImageSearch(string subscriptionKey)
+        {
+            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var imageResults = client.Images.SearchAsync(query: "canadian rockies").Result;
+                Console.WriteLine("Search images for query \"canadian rockies\"");
+
+                if (imageResults == null)
+                {
+                    Console.WriteLine("No image result data.");
+                }
+                else
+                {
+                    // Image results
+                    if (imageResults.Value.Count > 0)
+                    {
+                        var firstImageResult = imageResults.Value.First();
+
+                        Console.WriteLine($"Image result count: {imageResults.Value.Count}");
+                        Console.WriteLine($"First image insights token: {firstImageResult.ImageInsightsToken}");
+                        Console.WriteLine($"First image thumbnail url: {firstImageResult.ThumbnailUrl}");
+                        Console.WriteLine($"First image content url: {firstImageResult.ContentUrl}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find image results!");
+                    }
+                    
+                    Console.WriteLine($"Image result total estimated matches: {imageResults.TotalEstimatedMatches}");
+                    Console.WriteLine($"Image result next offset: {imageResults.NextOffset}");
+
+                    // Pivot suggestions
+                    if (imageResults.PivotSuggestions.Count > 0)
+                    {
+                        var firstPivot = imageResults.PivotSuggestions.First();
+
+                        Console.WriteLine($"Pivot suggestion count: {imageResults.PivotSuggestions.Count}");
+                        Console.WriteLine($"First pivot: {firstPivot.Pivot}");
+
+                        if (firstPivot.Suggestions.Count > 0)
+                        {
+                            var firstSuggestion = firstPivot.Suggestions.First();
+
+                            Console.WriteLine($"Suggestion count: {firstPivot.Suggestions.Count}");
+                            Console.WriteLine($"First suggestion text: {firstSuggestion.Text}");
+                            Console.WriteLine($"First suggestion web search url: {firstSuggestion.WebSearchUrl}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find suggestions!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find pivot suggestions!");
+                    }
+
+                    // Query expansions
+                    if (imageResults.QueryExpansions.Count > 0)
+                    {
+                        var firstQueryExpansion = imageResults.QueryExpansions.First();
+
+                        Console.WriteLine($"Query expansion count: {imageResults.QueryExpansions.Count}");
+                        Console.WriteLine($"First query expansion text: {firstQueryExpansion.Text}");
+                        Console.WriteLine($"First query expansion search link: {firstQueryExpansion.SearchLink}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find query expansions!");
+                    }
+                }
+            }
+          
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+            
+        }
+
+        [Example("This will search images for (studio ghibli), filtered for animated gifs and wide aspect, then verify number of results and print out insightsToken, thumbnail url and web url of first result")]
+        public static void ImageSearchWithFilters(string subscriptionKey)
+        {
+            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var imageResults = client.Images.SearchAsync(query: "studio ghibli", imageType: ImageType.AnimatedGif, aspect: ImageAspect.Wide).Result;
+                Console.WriteLine("Search images for \"studio ghibli\" results that are animated gifs and wide aspect");
+
+                if (imageResults == null)
+                {
+                    Console.WriteLine("Didn't see any image result data.");
+                }
+                else
+                {
+                    // First image result
+                    if (imageResults.Value.Count > 0)
+                    {
+                        var firstImageResult = imageResults.Value.First();
+
+                        Console.WriteLine($"Image result count: {imageResults.Value.Count}");
+                        Console.WriteLine($"First image insightsToken: {firstImageResult.ImageInsightsToken}");
+                        Console.WriteLine($"First image thumbnail url: {firstImageResult.ThumbnailUrl}");
+                        Console.WriteLine($"First image web search url: {firstImageResult.WebSearchUrl}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find image results!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+
+        }
+
+        [Example("This will search for trending images then verify categories and tiles")]
+        public static void ImageTrending(string subscriptionKey)
+        {
+            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var trendingResults = client.Images.TrendingAsync().Result;
+                Console.WriteLine("Search trending images");
+
+                if (trendingResults == null)
+                {
+                    Console.WriteLine("Didn't see any trending image data.");
+                }
+                else
+                {
+                    // Categories
+                    if (trendingResults.Categories?.Count > 0)
+                    {
+                        var firstCategory = trendingResults.Categories[0];
+                        Console.WriteLine($"Category count: {trendingResults.Categories.Count}");
+                        Console.WriteLine($"First category title: {firstCategory.Title}");
+                        
+                        // Tiles
+                        if (firstCategory.Tiles?.Count > 0)
+                        {
+                            var firstTile = firstCategory.Tiles[0];
+                            Console.WriteLine($"Tile count: {firstCategory.Tiles.Count}");
+                            Console.WriteLine($"First tile text: {firstTile.Query.Text}");
+                            Console.WriteLine($"First tile url: {firstTile.Query.WebSearchUrl}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find tiles!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find categories!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+
+        }
+
+        [Example("This will search images for (degas) and then search for image details of the first image")]
+        public static void ImageDetail(string subscriptionKey)
+        {
+            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var imageResults = client.Images.SearchAsync(query: "degas").Result;
+
+                var firstImage = imageResults?.Value?.FirstOrDefault();
+
+                if (firstImage != null)
+                {
+                    var modules = new List<string>() { ImageInsightModule.All };
+                    var imageDetail = client.Images.DetailsAsync(query: "degas", insightsToken: firstImage.ImageInsightsToken, modules: modules).Result;
+                    Console.WriteLine($"Search detail for image insightsToken={firstImage.ImageInsightsToken}");
+
+                    if (imageDetail != null)
+                    {
+                        // Insights token
+                        Console.WriteLine($"Expected image insights token: {imageDetail.ImageInsightsToken}");
+
+                        // Best representative query
+                        if (imageDetail.BestRepresentativeQuery != null)
+                        {
+                            Console.WriteLine($"Best representative query text: {imageDetail.BestRepresentativeQuery.Text}");
+                            Console.WriteLine($"Best representative query web search url: {imageDetail.BestRepresentativeQuery.WebSearchUrl}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find best representative query!");
+                        }
+
+                        // Caption 
+                        if (imageDetail.ImageCaption != null)
+                        {
+                            Console.WriteLine($"Image caption: {imageDetail.ImageCaption.Caption}");
+                            Console.WriteLine($"Image caption data source url: {imageDetail.ImageCaption.DataSourceUrl}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find image caption!");
+                        }
+
+                        // Pages including the image
+                        if (imageDetail.PagesIncluding?.Value?.Count > 0)
+                        {
+                            var firstPage = imageDetail.PagesIncluding.Value[0];
+                            Console.WriteLine($"Pages including count: {imageDetail.PagesIncluding.Value.Count}");
+                            Console.WriteLine($"First page content url: {firstPage.ContentUrl}");
+                            Console.WriteLine($"First page name: {firstPage.Name}");
+                            Console.WriteLine($"First page date published: {firstPage.DatePublished}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find any pages including this image!");
+                        }
+
+                        // Related searches 
+                        if (imageDetail.RelatedSearches?.Value?.Count > 0)
+                        {
+                            var firstRelatedSearch = imageDetail.RelatedSearches.Value[0];
+                            Console.WriteLine($"Related searches count: {imageDetail.RelatedSearches.Value.Count}");
+                            Console.WriteLine($"First related search text: {firstRelatedSearch.Text}");
+                            Console.WriteLine($"First related search web search url: {firstRelatedSearch.WebSearchUrl}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find any related searches!");
+                        }
+
+                        // Visually similar images
+                        if (imageDetail.VisuallySimilarImages?.Value?.Count > 0)
+                        {
+                            var firstVisuallySimilarImage = imageDetail.VisuallySimilarImages.Value[0];
+                            Console.WriteLine($"Visually similar images count: {imageDetail.RelatedSearches.Value.Count}");
+                            Console.WriteLine($"First visually similar image name: {firstVisuallySimilarImage.Name}");
+                            Console.WriteLine($"First visually similar image content url: {firstVisuallySimilarImage.ContentUrl}");
+                            Console.WriteLine($"First visually similar image size: {firstVisuallySimilarImage.ContentSize}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find any related searches!");
+                        }
+
+                        // Image tags
+                        if (imageDetail.ImageTags?.Value?.Count > 0)
+                        {
+                            var firstTag = imageDetail.ImageTags.Value[0];
+                            Console.WriteLine($"Image tags count: {imageDetail.ImageTags.Value.Count}");
+                            Console.WriteLine($"First tag name: {firstTag.Name}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Couldn't find any related searches!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find detail about the image!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Couldn't find image results!");
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+    }
+}
---
+++ SearchV7/VideoSearch/README.md
@@ -0,0 +1,28 @@
+# Bing Web Search SDK Samples
+
+These samples will show you how to get up and running using the SDKs for various Bing Search services. They'll cover a few rudimentary use cases and hopefully express best practices for interacting with the data from these APIs.
+
+## Features
+
+Please note that this samples package references an all-in-one SDK which includes all Bing Search services. Individual packages exist for each service if you would prefer working with smaller assembly sizes. Both individual service packages as well as the all-in-one will have feature parity for a particular service.
+
+This project framework provides examples for the Web Search service:
+
+* Using the **Bing Web Search SDK** \[[individual package](https://www.nuget.org/packages/Microsoft.Azure.CognitiveServices.Search.WebSearch/1.1.0-preview)\]\[[complete package](http://linktopackage)\] for the [Web Search API](https://azure.microsoft.com/en-us/services/cognitive-services/bing-web-search-api/)
+
+## Getting Started
+
+### Prerequisites
+
+- A cognitive services API key with which to authenticate the SDK's calls. [Sign up here](https://azure.microsoft.com/en-us/services/cognitive-services/directory/) by navigating to the **Search** services and acquiring an API key. You can get a trial key for **free** which will expire after 30 days.
+- .NET core SDK (ability to run .netcore 1.1 apps)
+
+### Quickstart
+
+To get these samples running locally, simply get the pre-requisites above, then:
+
+1. git clone https://github.com/Azure-Samples/cognitive-services-dotnet-sdk-samples.git
+2. cd cognitive-services-dotnet-sdk-samples\WebSearch
+3. open bing-search-dotnet.sln
+4. npm install [WebSearch](https://www.nuget.org/packages/Microsoft.Azure.CognitiveServices.Search.WebSearch/1.1.0-preview)
+5. navigate through the console app to play with examples
