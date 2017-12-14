221c60c711ec83b36a4c6a0664c49261cac4ff1e
xiaojinghe <hejing8608@hotmail.com>
2017-12-14 08:52:04 +0800

    Add news samples

---
+++ SearchV7/AllSearch/Samples/NewsSearchSamples.cs
@@ -0,0 +1,175 @@
+﻿namespace bing_search_dotnet.Samples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.Azure.CognitiveServices.Search.NewsSearch;
+    using Microsoft.Azure.CognitiveServices.Search.NewsSearch.Models;
+
+    [SampleCollection("NewsSearch")]
+    public class NewsSearchSamples
+    {
+        [Example("This will search news for (Quantum  Computing) with market and count parameters then verify number of results and print out totalEstimatedMatches, name, url, description, published time and name of provider of the first news result")]
+        public static void NewsSearch(string subscriptionKey)
+        {
+            var client = new NewsSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var newsResults = client.News.SearchAsync(query: "Quantum  Computing", market: "en-us", count: 10).Result;
+                Console.WriteLine("Search news for query \"Quantum  Computing\" with market and count");
+
+                if (newsResults == null)
+                {
+                    Console.WriteLine("Didn't see any news result data..");
+                }
+                else
+                {
+                    if (newsResults.Value.Count > 0)
+                    {
+                        var firstNewsResult = newsResults.Value.First();
+
+                        Console.WriteLine($"TotalEstimatedMatches value: {newsResults.TotalEstimatedMatches}");
+                        Console.WriteLine($"News result count: {newsResults.Value.Count}");
+                        Console.WriteLine($"First news name: {firstNewsResult.Name}");
+                        Console.WriteLine($"First news url: {firstNewsResult.Url}");
+                        Console.WriteLine($"First news description: {firstNewsResult.Description}");
+                        Console.WriteLine($"First news published time: {firstNewsResult.DatePublished}");
+                        Console.WriteLine($"First news provider: {firstNewsResult.Provider.First().Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find news results!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+
+        [Example("This will search most recent news for (Artificial Intelligence) with freshness and sortBy parameters then verify number of results and print out totalEstimatedMatches, name, url, description, published time and name of provider of the first news result")]
+        public static void NewsSearchWithFilters(string subscriptionKey)
+        {
+            var client = new NewsSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var newsResults = client.News.SearchAsync(query: "Artificial Intelligence", market: "en-us", freshness: "Week", sortBy: "Date").Result;
+                Console.WriteLine("Search most recent news for query \"Artificial Intelligence\" with freshness and sortBy");
+
+                if (newsResults == null)
+                {
+                    Console.WriteLine("Didn't see any news result data..");
+                }
+                else
+                {
+                    if (newsResults.Value.Count > 0)
+                    {
+                        var firstNewsResult = newsResults.Value.First();
+
+                        Console.WriteLine($"TotalEstimatedMatches value: {newsResults.TotalEstimatedMatches}");
+                        Console.WriteLine($"News result count: {newsResults.Value.Count}");
+                        Console.WriteLine($"First news name: {firstNewsResult.Name}");
+                        Console.WriteLine($"First news url: {firstNewsResult.Url}");
+                        Console.WriteLine($"First news description: {firstNewsResult.Description}");
+                        Console.WriteLine($"First news published time: {firstNewsResult.DatePublished}");
+                        Console.WriteLine($"First news provider: {firstNewsResult.Provider.First().Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find news results!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+
+        [Example("This will search category news for movie and TV entertainment with safe search then verify number of results and print out category, name, url, description, published time and name of provider of the first news result")]
+        public static void NewsCategory(string subscriptionKey)
+        {
+            var client = new NewsSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var newsResults = client.News.CategoryAsync(category: "Entertainment_MovieAndTV", market: "en-us", safeSearch: "strict").Result;
+                Console.WriteLine("Search category news for movie and TV entertainment with safe search");
+
+                if (newsResults == null)
+                {
+                    Console.WriteLine("Didn't see any news result data..");
+                }
+                else
+                {
+                    if (newsResults.Value.Count > 0)
+                    {
+                        var firstNewsResult = newsResults.Value.First();
+
+                        Console.WriteLine($"News result count: {newsResults.Value.Count}");
+                        Console.WriteLine($"First news category: {firstNewsResult.Category}");
+                        Console.WriteLine($"First news name: {firstNewsResult.Name}");
+                        Console.WriteLine($"First news url: {firstNewsResult.Url}");
+                        Console.WriteLine($"First news description: {firstNewsResult.Description}");
+                        Console.WriteLine($"First news published time: {firstNewsResult.DatePublished}");
+                        Console.WriteLine($"First news provider: {firstNewsResult.Provider.First().Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find news results!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+
+        [Example("This will search news trending topics in Bing then verify number of results and print out name, text of query, webSearchUrl, newsSearchUrl and image Url of the first news result")]
+        public static void TrendingTopics(string subscriptionKey)
+        {
+            var client = new NewsSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            try
+            {
+                var trendingTopics = client.News.TrendingAsync(market: "en-us").Result;
+                Console.WriteLine("Search news trending topics in Bing");
+
+                if (trendingTopics == null)
+                {
+                    Console.WriteLine("Didn't see any news trending topics..");
+                }
+                else
+                {
+                    if (trendingTopics.Value.Count > 0)
+                    {
+                        var firstTopic = trendingTopics.Value.First();
+
+                        Console.WriteLine($"Trending topics count: {trendingTopics.Value.Count}");
+                        Console.WriteLine($"First topic name: {firstTopic.Name}");
+                        Console.WriteLine($"First topic query: {firstTopic.Query.Text}")
+                        Console.WriteLine($"First topic image url: {firstTopic.Image.Url}");
+                        Console.WriteLine($"First topic webSearchUrl: {firstTopic.WebSearchUrl}");
+                        Console.WriteLine($"First topic newsSearchUrl: {firstTopic.NewsSearchUrl}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't find news trending topics!");
+                    }
+                }
+            }
+
+            catch (ErrorResponseException ex)
+            {
+                Console.WriteLine("Encountered exception. " + ex.Message);
+            }
+        }
+    }
+}
