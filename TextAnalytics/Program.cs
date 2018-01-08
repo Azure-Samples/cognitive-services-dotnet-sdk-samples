540ed6a44fc19611c9d633d38c0390994595d027
David Li <davl@microsoft.com>
2018-01-08 17:36:48 +0800

    Update Program.cs

--- TextAnalytics/Program.cs
+++ TextAnalytics/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using System.Collections.Generic;
