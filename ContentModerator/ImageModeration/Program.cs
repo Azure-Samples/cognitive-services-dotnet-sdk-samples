64f3eaf401fbeabf863c4d9173f7784fa29bca7d
sanjeev3 <sajagtap@microsoft.com>
2018-01-09 16:01:52 -0800

    Content Moderator Samples

---
+++ ContentModerator/ImageModeration/Program.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.CognitiveServices.ContentModerator;
+using Microsoft.CognitiveServices.ContentModerator.Models;
+using ModeratorHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace ImageModeration
+{
+    class Program
+    {
+        /// <summary>
+        /// Contains the image moderation results for an image, including 
+        /// text and face detection results.
+        /// </summary>
+        public class EvaluationData
+        {
+            /// <summary>
+            /// The URL of the evaluated image.
+            /// </summary>
+            public string ImageUrl;
+
+            /// <summary>
+            /// The image moderation results.
+            /// </summary>
+            public Evaluate ImageModeration;
+
+            /// <summary>
+            /// The text detection results.
+            /// </summary>
+            public OCR TextDetection;
+
+            /// <summary>
+            /// The face detection results;
+            /// </summary>
+            public FoundFaces FaceDetection;
+        }
+
+        /// <summary>
+        /// The name of the file that contains the image URLs to evaluate.
+        /// </summary>
+        /// <remarks>You will need to create an input file and update this path
+        /// accordingly. Relative paths are ralative the execution directory.</remarks>
+        private static string ImageUrlFile = "ImageFiles.txt";
+
+        /// <summary>
+        /// The name of the file to contain the output from the evaluation.
+        /// </summary>
+        /// <remarks>Relative paths are ralative the execution directory.</remarks>
+        private static string OutputFile = "ModerationOutput.json";
+
+        static void Main(string[] args)
+        {
+            // Create an object in which to store the image moderation results.
+            List<EvaluationData> evaluationData = new List<EvaluationData>();
+
+            // Create an instance of the Content Moderator API wrapper.
+            using (var client = Clients.NewClient())
+            {
+                // Read image URLs from the input file and evaluate each one.
+                using (StreamReader inputReader = new StreamReader(ImageUrlFile))
+                {
+                    while (!inputReader.EndOfStream)
+                    {
+                        string line = inputReader.ReadLine().Trim();
+                        if (line != String.Empty)
+                        {
+                            EvaluationData imageData = EvaluateImage(client, line);
+                            evaluationData.Add(imageData);
+                        }
+                    }
+                }
+            }
+
+            // Save the moderation results to a file.
+            using (StreamWriter outputWriter = new StreamWriter(OutputFile, false))
+            {
+                outputWriter.WriteLine(JsonConvert.SerializeObject(
+                    evaluationData, Formatting.Indented));
+
+                outputWriter.Flush();
+                outputWriter.Close();
+            }
+        }
+
+        /// <summary>
+        /// Evaluates an image using the Image Moderation APIs.
+        /// </summary>
+        /// <param name="client">The Content Moderator API wrapper to use.</param>
+        /// <param name="imageUrl">The URL of the image to evaluate.</param>
+        /// <returns>Aggregated image moderation results for the image.</returns>
+        /// <remarks>This method throttles calls to the API.
+        /// Your Content Moderator service key will have a requests per second (RPS)
+        /// rate limit, and the SDK will throw an exception with a 429 error code 
+        /// if you exceed that limit. A free tier key has a 1 RPS rate limit.
+        /// </remarks>
+        private static EvaluationData EvaluateImage(
+            ContentModeratorClient client, string imageUrl)
+        {
+            var url = new BodyModel("URL", imageUrl.Trim());
+
+            var imageData = new EvaluationData();
+
+            imageData.ImageUrl = url.Value;
+
+            // Evaluate for adult and racy content.
+            imageData.ImageModeration =
+                client.ImageModeration.EvaluateUrlInput("application/json", url, true);
+            Thread.Sleep(1000);
+
+            // Detect and extract text.
+            imageData.TextDetection =
+                client.ImageModeration.OCRUrlInput("eng", "application/json", url, true);
+            Thread.Sleep(1000);
+
+            // Detect faces.
+            imageData.FaceDetection =
+                client.ImageModeration.FindFacesUrlInput("application/json", url, true);
+            Thread.Sleep(1000);
+
+            return imageData;
+        }
+    }
+}
---
+++ ContentModerator/ImageReviewJobs/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.CognitiveServices.ContentModerator.Models;
+using ModeratorHelper;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace ImageReviewJobs
+{
+    class Program
+    {
+        /// <summary>
+        /// The moderation job will use this workflow that you defined earlier.
+        /// See the quickstart article to learn how to setup custom workflows.
+        /// </summary>
+        private const string WorkflowName = "OCR";
+
+        /// <summary>
+        /// The name of the team to assign the job to.
+        /// </summary>
+        /// <remarks>This must be the team name you used to create your 
+        /// Content Moderator account. You can retrieve your team name from
+        /// the Conent Moderator web site. Your team name is the Id associated 
+        /// with your subscription.</remarks>
+        private const string TeamName = "testreview6";
+
+        /// <summary>
+        /// The URL of the image to create a review job for.
+        /// </summary>
+        private const string ImageUrl =
+            "https://moderatorsampleimages.blob.core.windows.net/samples/sample2.jpg";
+
+        /// <summary>
+        /// The name of the log file to create.
+        /// </summary>
+        /// <remarks>Relative paths are ralative the execution directory.</remarks>
+        private const string OutputFile = "OutputLog.txt";
+
+        /// <summary>
+        /// The number of seconds to delay after a review has finished before
+        /// getting the review results from the server.
+        /// </summary>
+        private const int latencyDelay = 45;
+
+        /// <summary>
+        /// The callback endpoint for completed reviews.
+        /// </summary>
+        /// <remarks>Revies show up for reviewers on your team. 
+        /// As reviewers complete reviews, results are sent to the
+        /// callback endpoint using an HTTP POST request.</remarks>
+        private const string CallbackEndpoint = "https://requestb.in/vxke1mvx";
+
+        static void Main(string[] args)
+        {
+            using (TextWriter writer = new StreamWriter(OutputFile, false))
+            {
+                using (var client = Clients.NewClient())
+                {
+                    writer.WriteLine("Create moderation job for an image.");
+                    var content = new Content(ImageUrl);
+
+                    // The WorkflowName contains the nameof the workflow defined in the online review tool.
+                    // See the quickstart article to learn more.
+                    var jobResult = client.Reviews.CreateJobWithHttpMessagesAsync(
+                        TeamName, "image", "contentID", WorkflowName, "application/json", content, CallbackEndpoint);
+
+                    // Record the job ID.
+                    var jobId = jobResult.Result.Body.JobIdProperty;
+
+                    // Log just the response body from the returned task.
+                    writer.WriteLine(JsonConvert.SerializeObject(
+                        jobResult.Result.Body, Formatting.Indented));
+
+                    Thread.Sleep(2000);
+                    writer.WriteLine();
+
+                    writer.WriteLine("Get job status before review.");
+                    var jobDetails = client.Reviews.GetJobDetailsWithHttpMessagesAsync(
+                        TeamName, jobId);
+
+                    // Log just the response body from the returned task.
+                    writer.WriteLine(JsonConvert.SerializeObject(
+                        jobDetails.Result.Body, Formatting.Indented));
+
+                    Console.WriteLine();
+                    Console.WriteLine("Perform manual reviews on the Content Moderator site.");
+                    Console.WriteLine("Then, press any key to continue.");
+                    Console.ReadKey();
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Waiting {latencyDelay} seconds for results to propagate.");
+                    Thread.Sleep(latencyDelay * 1000);
+
+                    writer.WriteLine("Get job status after review.");
+                    jobDetails = client.Reviews.GetJobDetailsWithHttpMessagesAsync(
+                        TeamName, jobId);
+
+                    // Log just the response body from the returned task.
+                    writer.WriteLine(JsonConvert.SerializeObject(
+                        jobDetails.Result.Body, Formatting.Indented));
+                }
+                writer.Flush();
+                writer.Close();
+            }
+        }
+    }
+}
