423431241282682c646e319f39dd2a2c5f251989
Yang Yuan <yangyuan@users.noreply.github.com>
2018-10-22 11:05:56 -0700

    [Face] Adjust Face examples to fit the new testable design. (#78)
    * Adjust Face examples to fit the new testable design.
    
    * Add configurations for Face in tester.

---
+++ samples/Face/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.Azure.CognitiveServices.Samples.Face
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a client.
+            string apiKey = "ENTER YOUR KEY HERE";
+            string endpoint = "ENTER YOUR ENDPOINT HERE";
+
+            VerifyFaceToFace.Run(endpoint, apiKey).Wait();
+
+            Console.WriteLine("\nPress ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
--- samples/Face/VerifyFaceToFace.cs
+++ samples/Face/VerifyFaceToFace.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Microsoft.Azure.CognitiveServices.Vision.Face;
-using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
-
-namespace VerifyFaceToFace
+﻿namespace Microsoft.Azure.CognitiveServices.Samples.Face
 {
-    public class Program
+    using Microsoft.Azure.CognitiveServices.Vision.Face;
+    using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public static class VerifyFaceToFace
     {
-        public static void Main(string[] args)
+        public static async Task Run(string endpoint, string key)
         {
-            // Create a client.
-            string apiKey = "ENTER YOUR KEY HERE";
-            IFaceClient client = new FaceClient(new ApiKeyServiceClientCredentials(apiKey))
+            IFaceClient client = new FaceClient(new ApiKeyServiceClientCredentials(key))
             {
-                Endpoint = "ENTER YOUR ENDPOINT HERE"
+                Endpoint = endpoint
             };
 
             List<string> imageFileNames =
-                new List<string> { "Family1-Dad1.jpg", "Family1-Dad2.jpg", "Family1-Son1.jpg" };
+             new List<string> { "Family1-Dad1.jpg", "Family1-Dad2.jpg", "Family1-Son1.jpg" };
             List<Guid> faceIds = new List<Guid>();
 
             foreach (var imageFileName in imageFileNames)
@@ -27,18 +26,16 @@ namespace VerifyFaceToFace
                 using (FileStream stream = new FileStream(Path.Combine("Images", imageFileName), FileMode.Open))
                 {
                     // Detect faces from image stream.
-                    IList<DetectedFace> detectedFaces = client.Face.DetectWithStreamAsync(stream).Result;
+                    IList<DetectedFace> detectedFaces = await client.Face.DetectWithStreamAsync(stream);
                     if (detectedFaces == null || detectedFaces.Count == 0)
                     {
-                        Console.WriteLine($"[Error] No face detected from image `{imageFileName}`.");
-                        return;
+                        throw new Exception($"No face detected from image `{imageFileName}`.");
                     }
 
                     Console.WriteLine($"{detectedFaces.Count} faces detected from image `{imageFileName}`.");
                     if (detectedFaces[0].FaceId == null)
                     {
-                        Console.WriteLine("[Error] Parameter `returnFaceId` of `DetectWithStreamAsync` must be set to `true` (by default) for verification purpose.");
-                        return;
+                        throw new Exception($"Parameter `returnFaceId` of `DetectWithStreamAsync` must be set to `true` (by default) for verification purpose.");
                     }
 
                     faceIds.Add(detectedFaces[0].FaceId.Value);
@@ -46,21 +43,18 @@ namespace VerifyFaceToFace
             }
 
             // Verification example for faces of the same person.
-            VerifyResult verifyResult1 = client.Face.VerifyFaceToFaceAsync(faceIds[0], faceIds[1]).Result;
+            VerifyResult verifyResult1 = await client.Face.VerifyFaceToFaceAsync(faceIds[0], faceIds[1]);
             Console.WriteLine(
                 verifyResult1.IsIdentical
                     ? $"Faces from {imageFileNames[0]} & {imageFileNames[1]} are of the same (Positive) person, similarity confidence: {verifyResult1.Confidence}."
                     : $"Faces from {imageFileNames[0]} & {imageFileNames[1]} are of different (Negative) persons, similarity confidence: {verifyResult1.Confidence}.");
 
             // Verification example for faces of different persons.
-            VerifyResult verifyResult2 = client.Face.VerifyFaceToFaceAsync(faceIds[1], faceIds[2]).Result;
+            VerifyResult verifyResult2 = await client.Face.VerifyFaceToFaceAsync(faceIds[1], faceIds[2]);
             Console.WriteLine(
                 verifyResult2.IsIdentical
                     ? $"Faces from {imageFileNames[1]} & {imageFileNames[2]} are of the same (Negative) person, similarity confidence: {verifyResult2.Confidence}."
                     : $"Faces from {imageFileNames[1]} & {imageFileNames[2]} are of different (Positive) persons, similarity confidence: {verifyResult2.Confidence}.");
-
-            Console.WriteLine("\nPress ENTER to exit.");
-            Console.ReadLine();
         }
     }
 }
