2bfd865c91c62b295050147ce43626456db31566
Siqi Liu <siqliu@microsoft.com>
2018-10-30 01:43:56 +0800

    [Face] Add full set of Face DotNet samples. (#83)
    * Add full set of Face DotNet samples.
    * Change to load image from url instead of file path, nit code style/documentation changes.

--- samples/Face/Program.cs
+++ samples/Face/Program.cs
@@ -10,7 +10,25 @@
             string apiKey = "ENTER YOUR KEY HERE";
             string endpoint = "ENTER YOUR ENDPOINT HERE";
 
+            // Detect sample.
+            Detection.Run(endpoint, apiKey).Wait();
+
+            // FindSimilar samples.
+            FindSimilarInFaceIds.Run(endpoint, apiKey).Wait();
+            FindSimilarInFaceList.Run(endpoint, apiKey).Wait();
+            FindSimilarInLargeFaceList.Run(endpoint, apiKey).Wait();
+
+            // Group sample.
+            Group.Run(endpoint, apiKey).Wait();
+
+            // Identify sample.
+            IdentifyInPersonGroup.Run(endpoint, apiKey).Wait();
+            IdentifyInLargePersonGroup.Run(endpoint, apiKey).Wait();
+
+            // Verify samples.
             VerifyFaceToFace.Run(endpoint, apiKey).Wait();
+            VerifyInPersonGroup.Run(endpoint, apiKey).Wait();
+            VerifyInLargePersonGroup.Run(endpoint, apiKey).Wait();
 
             Console.WriteLine("\nPress ENTER to exit.");
             Console.ReadLine();
