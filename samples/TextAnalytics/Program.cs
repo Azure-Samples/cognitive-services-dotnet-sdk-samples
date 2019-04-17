e5b019d03a9d48544093eb153b53f22b13681869
Assaf Israel <assafi@microsoft.com>
2019-04-17 15:55:16 -0700

    Update Text Analytics .net SDK

--- samples/TextAnalytics/Program.cs
+++ samples/TextAnalytics/Program.cs
@@ -1,113 +1,123 @@
-using System;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+    using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+    using Microsoft.Rest;
 
 namespace Microsoft.Azure.CognitiveServices.Samples.TextAnalytics
 {
+    /// <summary>
+    /// Allows authentication to the API using a basic apiKey mechanism
+    /// </summary>
+    class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private readonly string subscriptionKey;
+
+        /// <summary>
+        /// Creates a new instance of the ApiKeyServiceClientCredentails class
+        /// </summary>
+        /// <param name="subscriptionKey">The subscription key to authenticate and authorize as</param>
+        public ApiKeyServiceClientCredentials(string subscriptionKey)
+        {
+            this.subscriptionKey = subscriptionKey;
+        }
+
+        /// <summary>
+        /// Add the Basic Authentication Header to each outgoing request
+        /// </summary>
+        /// <param name="request">The outgoing request</param>
+        /// <param name="cancellationToken">A token to cancel the operation</param>
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
+            request.Headers.Add("Ocp-Apim-Subscription-Key", this.subscriptionKey);
+
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+
     public static class Program
     {
-        static async Task Main(string[] args)
+        private const string SubscriptionKey = "3e4ad3e73b5f45cc81c592ab6a15e68f";
+
+        static void Main(string[] args)
         {
-            await RunSampleAsync(
-                endpoint: "https://westus.api.cognitive.microsoft.com", //Replace 'westus' with the correct region for your Text Analytics subscription
-                key: ""); // Replace with Cognitive Services key
-            Console.WriteLine("Press ENTER to exit.");
+            var credentials = new ApiKeyServiceClientCredentials(SubscriptionKey);
+            var client = new TextAnalyticsClient(credentials)
+            {
+                //Replace 'westus' with the correct region for your Text Analytics subscription
+                Endpoint = "https://eastus.api.cognitive.microsoft.com"
+            };
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            SentimentAnalysisExample(client).Wait();
+            DetectLanguageExample(client).Wait();
+            RecognizeEntitiesExample(client).Wait();
+            KeyPhraseExtractionExample(client).Wait();
             Console.ReadLine();
         }
 
-        public static async Task RunSampleAsync(string endpoint, string key)
+        public static async Task SentimentAnalysisExample(TextAnalyticsClient client)
         {
-            // Create a client.
-            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials(key))
+
+            // The documents to be analyzed. Add the language of the document. The ID can be any value.
+            var inputDocuments = new MultiLanguageBatchInput(
+                new List<MultiLanguageInput>
+                {
+                    new MultiLanguageInput("en", "1", "I had the best day of my life."),
+                    new MultiLanguageInput("en", "2", "This was a waste of my time. The speaker put me to sleep."),
+                    new MultiLanguageInput("es", "3", "No tengo dinero ni nada que dar..."),
+                    new MultiLanguageInput("it", "4", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."),
+                });
+
+            var result = await client.SentimentAsync(false, inputDocuments);
+
+            // Printing sentiment results
+            foreach (var document in result.Documents)
             {
-                Endpoint = endpoint
-            }; 
+                Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
+            }
+        }
 
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-            // Extracting language
-            Console.WriteLine("===== LANGUAGE EXTRACTION ======");
-
-            var langResults = await client.DetectLanguageAsync(
-                false,
-                new LanguageBatchInput(
+        public static async Task DetectLanguageExample(TextAnalyticsClient client)
+        {
+            // The documents to be submitted for language detection. The ID can be any value.
+            var inputDocuments = new LanguageBatchInput(
                     new List<LanguageInput>
                         {
-                          new LanguageInput(id: "1", text: "This is a document written in English."),
-                          new LanguageInput(id: "2", text: "Este es un document escrito en Español."),
-                          new LanguageInput(id: "3", text: "这是一个用中文写的文件")
-                        }));
+                            new LanguageInput(id: "1", text: "This is a document written in English."),
+                            new LanguageInput(id: "2", text: "Este es un document escrito en Español."),
+                            new LanguageInput(id: "3", text: "这是一个用中文写的文件")
+                        });
 
-            // Printing language results.
+            var langResults = await client.DetectLanguageAsync(false, inputDocuments);
+
+            // Printing detected languages
             foreach (var document in langResults.Documents)
             {
                 Console.WriteLine($"Document ID: {document.Id} , Language: {document.DetectedLanguages[0].Name}");
             }
+        }
 
-            // Getting key-phrases
-            Console.WriteLine("\n\n===== KEY-PHRASE EXTRACTION ======");
-
-            var kpResults = await client.KeyPhrasesAsync(
-                false,
-                new MultiLanguageBatchInput(
-                    new List<MultiLanguageInput>
-                    {
-                        new MultiLanguageInput("ja", "1", "猫は幸せ"),
-                        new MultiLanguageInput("de", "2", "Fahrt nach Stuttgart und dann zum Hotel zu Fu."),
-                        new MultiLanguageInput("en", "3", "My cat is stiff as a rock."),
-                        new MultiLanguageInput("es", "4", "A mi me encanta el fútbol!")
-                    }));
-
-            // Printing keyphrases
-            foreach (var document in kpResults.Documents)
-            {
-                Console.WriteLine($"Document ID: {document.Id} ");
-
-                Console.WriteLine("\t Key phrases:");
-
-                foreach (string keyphrase in document.KeyPhrases)
+        public static async Task RecognizeEntitiesExample(TextAnalyticsClient client)
+        {
+            // The documents to be submitted for entity recognition. The ID can be any value.
+            var inputDocuments = new MultiLanguageBatchInput(
+                new List<MultiLanguageInput>
                 {
-                    Console.WriteLine($"\t\t{keyphrase}");
-                }
-            }
+                    new MultiLanguageInput("en", "1", "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, to develop and sell BASIC interpreters for the Altair 8800."),
+                    new MultiLanguageInput("es", "2", "La sede principal de Microsoft se encuentra en la ciudad de Redmond, a 21 kilómetros de Seattle.")
+                });
 
-            // Extracting sentiment
-            Console.WriteLine("\n\n===== SENTIMENT ANALYSIS ======");
+            var entitiesResult = await client.EntitiesAsync(false, inputDocuments);
 
-            var sentimentResults = await client.SentimentAsync(
-                false,
-                new MultiLanguageBatchInput(
-                    new List<MultiLanguageInput>
-                    {
-                        new MultiLanguageInput("en", "1", "I had the best day of my life."),
-                        new MultiLanguageInput("en", "2", "This was a waste of my time. The speaker put me to sleep."),
-                        new MultiLanguageInput("es", "3", "No tengo dinero ni nada que dar..."),
-                        new MultiLanguageInput("it", "4", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."),
-                    }));
-
-
-            // Printing sentiment results
-            foreach (var document in sentimentResults.Documents)
-            {
-                Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
-            }
-
-
-            // Identify entities
-            Console.WriteLine("\n\n===== ENTITIES ======");
-
-            var entitiesResult = await client.EntitiesAsync(
-                false,
-                new MultiLanguageBatchInput(
-                    new List<MultiLanguageInput>()
-                    {
-                        new MultiLanguageInput("en", "1", "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, to develop and sell BASIC interpreters for the Altair 8800."),
-                        new MultiLanguageInput("es", "2", "La sede principal de Microsoft se encuentra en la ciudad de Redmond, a 21 kilómetros de Seattle.")
-                    }));
-
-            // Printing entities results
+            // Printing recognized entities
             foreach (var document in entitiesResult.Documents)
             {
                 Console.WriteLine($"Document ID: {document.Id} ");
@@ -121,6 +131,33 @@ namespace Microsoft.Azure.CognitiveServices.Samples.TextAnalytics
                     {
                         Console.WriteLine($"\t\t\tOffset: {match.Offset},\tLength: {match.Length},\tScore: {match.EntityTypeScore:F3}");
                     }
+                }
+            }
+        }
+
+        public static async Task KeyPhraseExtractionExample(TextAnalyticsClient client)
+        {
+            var inputDocuments = new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>
+                        {
+                            new MultiLanguageInput("ja", "1", "猫は幸せ"),
+                            new MultiLanguageInput("de", "2", "Fahrt nach Stuttgart und dann zum Hotel zu Fu."),
+                            new MultiLanguageInput("en", "3", "My cat might need to see a veterinarian."),
+                            new MultiLanguageInput("es", "4", "A mi me encanta el fútbol!")
+                        });
+
+            var kpResults = await client.KeyPhrasesAsync(false, inputDocuments);
+
+            // Printing keyphrases
+            foreach (var document in kpResults.Documents)
+            {
+                Console.WriteLine($"Document ID: {document.Id} ");
+
+                Console.WriteLine("\t Key phrases:");
+
+                foreach (string keyphrase in document.KeyPhrases)
+                {
+                    Console.WriteLine($"\t\t{keyphrase}");
                 }
             }
         }
--- samples/TextAnalytics/README.md
+++ samples/TextAnalytics/README.md
@@ -14,24 +14,15 @@ You must also have the [endpoint and access key](../How-tos/text-analytics-how-t
 
 ## Running the project
 1. Open the TextAnalyticsSample.sln file in Visual Studio 2017.
-1. Replace the subscription key stub in line 15 with the key you obtain from your free account.
+1. Replace the subscription key stub with the key you obtain from your free account.
 1. Ensure that region is set to the Azure region associated with the free account.
 1. Run the solution.
 
 > [!Tip]
->  While you could call the [HTTP endpoints](https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c6) directly from C#, the Microsoft.Azure.CognitiveServices.Language SDK makes it much easier to call the service without having to worry about serializing and deserializing JSON.
+>  While you could call the [HTTP endpoints](https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics-v2-1/operations/56f30ceeeda5650db055a3c9) directly from C#, the Microsoft.Azure.CognitiveServices.Language SDK makes it much easier to call the service without having to worry about serializing and deserializing JSON.
 >
 > A few useful links:
-> - [SDK Nuget page](https://www.nuget.org/packages/Microsoft.Azure.CognitiveServices.Language)
-> - [SDK code ](https://github.com/Azure/azure-sdk-for-net/tree/psSdkJson6/src/SDKs/CognitiveServices/dataPlane/Language/Language)
+> - [SDK Nuget page](https://www.nuget.org/packages/Microsoft.Azure.CognitiveServices.Language.TextAnalytics)
+> - [SDK code ](https://github.com/Azure/azure-sdk-for-net/tree/master/src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics)
 
-## Next steps
-
-> [!div class="nextstepaction"]
-> [Text Analytics With Power BI](../tutorials/tutorial-power-bi-key-phrases.md)
-
-## See also 
-
- [Text Analytics overview](../overview.md)  
- [Frequently asked questions (FAQ)](../text-analytics-resource-faq.md)
 
