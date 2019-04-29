628a085bd9ae189232b6816280f61f2972e9090c
Siqi Liu <siqliu@microsoft.com>
2019-04-29 12:58:00 +0800

    [Face] Add head pose sample in real time video scenario (#106)
    * Add headpose sample.
    
    * Change path.
    
    * Set returnFaceId to false.
    
    * Fix style issue.
    
    * Update fix style issue.
    
    * Update style issue.
    
    * Add readme.
    
    * update readme.
    
    * Update readme and Ignore time out message.

---
+++ app-samples/FaceAPIHeadPoseSample/FaceAPIHeadPoseSample/VideoFrame.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace FaceAPIHeadPoseSample
+{
+    public struct VideoFrameMetadata
+    {
+        public DateTime Timestamp;
+        public int Index;
+    }
+
+    /// <summary> A video frame produced by the <see cref="FrameGrabber{AnalysisResultType}"/>.
+    ///     This class encapsulates the image, any metadata, and also allows the user to attach
+    ///     some arbitrary data to each frame as it flows through the pipeline. </summary>
+    public class VideoFrame
+    {
+        /// <summary> Constructor. </summary>
+        /// <param name="image">    The image captured by the camera. </param>
+        /// <param name="metadata"> The metadata. </param>
+        public VideoFrame(Mat image, VideoFrameMetadata metadata)
+        {
+            Image = image;
+            Metadata = metadata;
+        }
+
+        /// <summary> Gets the image for the frame. </summary>
+        /// <value> The image. </value>
+        public Mat Image { get; }
+
+        /// <summary> Gets the frame's metadata. </summary>
+        /// <value> The metadata. </value>
+        public VideoFrameMetadata Metadata { get; }
+
+        /// <summary> Gets or sets the frame's "user data". </summary>
+        /// <value> Any additional data that the user would like to attach to a video frame. </value>
+        public object UserData { get; set; } = null;
+    }
+}
