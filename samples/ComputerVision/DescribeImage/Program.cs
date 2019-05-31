332c67f438b949f3d5d96a5e8a471f40dc4dcd00
Harish <hakr@microsoft.com>
2019-05-31 13:58:15 -0700

    Added two quickstarts: tag image and describe image Modified ExtractText and OCR to be consistent with rest API samples.

---
+++ samples/ComputerVision/DescribeImage/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.CognitiveServices.Samples.ComputerVision.DescribeImage
+{
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+    class Program
+    {
+        public const string subscriptionKey = "<your training key here>"; //Insert your Cognitive Services subscription key here
+        public const string endpoint = "https://westus.api.cognitive.microsoft.com"; // You must use the same Azure region that you generated your subscription keys for.  Free trial subscription keys are generated in the westus region. 
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                DescribeImageSample.RunAsync(endpoint, subscriptionKey).Wait(5000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("\nPress ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+    public class DescribeImageSample
+    {
+        public static async Task RunAsync(string endpoint, string key)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            {
+                Endpoint = endpoint
+            };
+
+            string localImagePath = @"Images\objects.jpg";   // See this repo's readme.md for info on how to get these images. Alternatively, you can just set the path to any appropriate image on your machine.
+            string remoteImageUrl = "https://github.com/Azure-Samples/cognitive-services-sample-data-files/raw/master/ComputerVision/Images/celebrities.jpg";
+
+            Console.WriteLine("Image description");
+            await DescribeImageFromUrlAsync(computerVision, remoteImageUrl);
+            await DescribeImageFromStreamAsync(computerVision, localImagePath);
+        }
+
+        // Analyze a remote image
+        private static async Task DescribeImageFromUrlAsync(ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("\nInvalid remote image url:\n{0} \n", imageUrl);
+                return;
+            }
+
+            ImageDescription descriptions = await computerVision.DescribeImageAsync(imageUrl);
+
+            Console.WriteLine(imageUrl);
+            DisplayDescriptions(descriptions);
+        }
+
+        // Analyze a local image
+        private static async Task DescribeImageFromStreamAsync(ComputerVisionClient computerVision, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine("\nUnable to open or read local image path:\n{0} \n", imagePath);
+                return;
+            }
+
+            using (Stream imageStream = File.OpenRead(imagePath))
+            {
+                ImageDescription descriptions = await computerVision.DescribeImageInStreamAsync(imageStream);
+                Console.WriteLine(imagePath);
+                DisplayDescriptions(descriptions);
+            }
+        }
+        private static void DisplayDescriptions(ImageDescription descriptions)
+        {
+            foreach (var description in descriptions.Captions)
+            {
+                Console.WriteLine("{0}\t\t with confidence {1}", description.Text, description.Confidence);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
