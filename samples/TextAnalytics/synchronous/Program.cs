497927c9cb7f77043576cf1a90a4efdbe8bf635e
Aaron Hill <aahi@microsoft.com>
2019-11-26 11:10:01 -0800

    removing env variables

--- samples/TextAnalytics/synchronous/Program.cs
+++ samples/TextAnalytics/synchronous/Program.cs
@@ -14,24 +14,9 @@ namespace text_analytics_quickstart
     class Program
     {
         // <vars>
-        private const string key_var = "TEXT_ANALYTICS_SUBSCRIPTION_KEY";
-        private static readonly string key = Environment.GetEnvironmentVariable(key_var);
-
-        private const string endpoint_var = "TEXT_ANALYTICS_ENDPOINT";
-        private static readonly string endpoint = Environment.GetEnvironmentVariable(endpoint_var);
+        private static readonly string key = "<paste-your-text-analytics-key-here>";
+        private static readonly string endpoint = "<paste-your-text-analytics-endpoint-here>";
         // </vars>
-
-        static Program()
-        {
-            if (null == key)
-            {
-                throw new Exception("Please set/export the environment variable: " + key_var);
-            }
-            if (null == endpoint)
-            {
-                throw new Exception("Please set/export the environment variable: " + endpoint_var);
-            }
-        }
 
         // <authentication>
         static TextAnalyticsClient authenticateClient()
