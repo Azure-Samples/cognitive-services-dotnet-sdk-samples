d8084a13693158e16fc4cf0ce1ee3395e55b5e27
Aaron Hill <aahi@microsoft.com>
2020-03-09 11:47:22 -0700

    updating client

--- BingSearchv7/BingImageSearch/multifeatured/ImageSearchSamples.cs
+++ BingSearchv7/BingImageSearch/multifeatured/ImageSearchSamples.cs
@@ -12,7 +12,7 @@
         [Example("This will search images for (canadian rockies) then verify number of results and print out first image result, pivot suggestion, and query expansion")]
         public static void ImageSearch(string subscriptionKey)
         {
-            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+            var client = new ImageSearchClient(new ApiKeyServiceClientCredentials(subscriptionKey));
 
             try
             {
@@ -95,7 +95,7 @@
         [Example("This will search images for (studio ghibli), filtered for animated gifs and wide aspect, then verify number of results and print out insightsToken, thumbnail url and web url of first result")]
         public static void ImageSearchWithFilters(string subscriptionKey)
         {
-            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+            var client = new ImageSearchClient(new ApiKeyServiceClientCredentials(subscriptionKey));
 
             try
             {
@@ -135,7 +135,7 @@
         [Example("This will search for trending images then verify categories and tiles")]
         public static void ImageTrending(string subscriptionKey)
         {
-            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+            var client = new ImageSearchClient(new ApiKeyServiceClientCredentials(subscriptionKey));
 
             try
             {
@@ -185,7 +185,7 @@
         [Example("This will search images for (degas) and then search for image details of the first image")]
         public static void ImageDetail(string subscriptionKey)
         {
-            var client = new ImageSearchAPI(new ApiKeyServiceClientCredentials(subscriptionKey));
+            var client = new ImageSearchClient(new ApiKeyServiceClientCredentials(subscriptionKey));
 
             try
             {
