2dd2fb3ae76b3a971fadfff038c5b3d7deef5c77
Tianyun Gao <v-tianga@microsoft.com>
2021-02-02 15:01:00 +0800

    Change comment

--- samples/TextAnalytics/asynchronous/LanguageDetectionSample.cs
+++ samples/TextAnalytics/asynchronous/LanguageDetectionSample.cs
@@ -2,38 +2,30 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
-
-namespace Microsoft.Azure.CognitiveServices.Samples.TextAnalytics
+namespace Azure.AI.TextAnalytics.Samples
 {
     public static class LanguageDetectionSample
     {
-        public static async Task RunAsync(string endpoint, string key)
+        public static async Task RunAsync(string endpoint, string apiKey)
         {
-            var credentials = new ApiKeyServiceClientCredentials(key);
-            var client = new TextAnalyticsClient(credentials)
-            {
-                Endpoint = endpoint
-            };
+            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
 
             // The documents to be submitted for language detection. The ID can be any value.
-            var inputDocuments = new LanguageBatchInput(
-                    new List<LanguageInput>
+            var inputDocuments = new List<DetectLanguageInput>
                         {
-                            new LanguageInput("1", "This is a document written in English."),
-                            new LanguageInput("2", "Este es un document escrito en Español."),
-                            new LanguageInput("3", "这是一个用中文写的文件")
-                        });
+                            new DetectLanguageInput("1", "This is a document written in English."),
+                            new DetectLanguageInput("2", "Este es un document escrito en Español."),
+                            new DetectLanguageInput("3", "这是一个用中文写的文件")
+                        };
 
             var langResults = await client.DetectLanguageBatchAsync(inputDocuments);
 
             // Printing detected languages
             Console.WriteLine("===== Language Detection =====\n");
 
-            foreach (var document in langResults.Documents)
+            foreach (var document in langResults.Value)
             {
-                Console.WriteLine($"Document ID: {document.Id} , Language: {document.DetectedLanguages[0].Name}");
+                Console.WriteLine($"Document ID: {document.Id} , Language: {document.PrimaryLanguage.Name}");
             }
             Console.WriteLine();
         }
