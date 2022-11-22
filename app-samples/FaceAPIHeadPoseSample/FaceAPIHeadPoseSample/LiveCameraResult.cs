5428f627dd70ad29214fa2c9155aa8ab0b90149c
Patrick Farley <pafarley@microsoft.com>
2022-11-22 16:19:09 -0500

    merge from master

--- app-samples/FaceAPIHeadPoseSample/FaceAPIHeadPoseSample/LiveCameraResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
-
-namespace FaceAPIHeadPoseSample
-{
-    public class LiveCameraResult
-    {
-        public DetectedFace[] Faces { get; set; } = null;
-    }
-}
